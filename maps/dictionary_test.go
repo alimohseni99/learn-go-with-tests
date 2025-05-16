759f518c8ac90551246a8886779cde8978ad12a0
Ali Mohseni <ali.mohseni05@yahoo.se>
2025-05-16 16:02:22 +0200

    Refactor Search test in dictionary; introduce assertion helper for improved readability and maintainability

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -4,11 +4,15 @@ import "testing"
 
 func TestSearch(t *testing.T){
 	dictionary := map[string]string{"test": "this is just a test"}
-
 	got := Search(dictionary, "test")
 	want := "this is just a test"
 
-	if got != want {
+	assertString(t, got, want)
+}
+
+func assertString(t testing.TB, got, want string){
+	t.Helper()
+		if got != want {
 		t.Errorf("got %q want %q given %q", got, want, "test")
 	}
 }
