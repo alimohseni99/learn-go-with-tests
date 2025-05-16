bbe4e5d58156829a0b02d87c284780ec697dd7b9
Ali Mohseni <ali.mohseni05@yahoo.se>
2025-05-16 16:19:30 +0200

    Update dictionary tests to include subtests for known words; refactor Search function to use Dictionary type for improved structure

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,11 +3,13 @@ package maps
 import "testing"
 
 func TestSearch(t *testing.T){
+	t.Run("known word", func(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 	got := dictionary.Search("test")
 	want := "this is just a test"
-
 	assertString(t, got, want)
+	})
+	
 }
 
 func assertString(t testing.TB, got, want string){
