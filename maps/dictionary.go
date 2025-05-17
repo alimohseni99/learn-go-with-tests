4dca75210894cca37e89b1d90e04709f30de09f3
Ali Mohseni <ali.mohseni05@yahoo.se>
2025-05-17 13:31:00 +0200

    Refactor dictionary error handling; update tests to use consistent error variables and improve test coverage for Add function

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,20 +1,40 @@
 package maps
 
-import "errors"
+import (
+	"errors"
+)
 
 type Dictionary map[string]string
+type DictionaryErr string
 
-var ErrorNotFound =  errors.New("could not find the word you were looking for")
+var (
+	ErrNotFound   = errors.New("could not find the word you were looking for")
+	ErrWordExists = errors.New("cannot add word because it already exists")
+)
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Search(word string)(string, error) {
 	definition, ok := d[word]
 	if !ok {
-		return "", ErrorNotFound
+		return "", ErrNotFound
 	}
 	return definition, nil
 }
 
-func (d Dictionary) Add(word, definition string){
-				d[word] = definition
+func (d Dictionary) Add(word, definition string)	error{
 
+		_, err := d.Search(word)
+
+		switch err {
+			case ErrNotFound:
+					d[word] = definition
+			case nil:
+					return ErrWordExists
+			default:
+					return err
+		}
+				return nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -18,18 +18,31 @@ func TestSearch(t *testing.T){
 		if got == nil {
 			t.Fatal("expected to get an error")
 		}
-		assertError(t, got, ErrorNotFound)
+		assertError(t, got, ErrNotFound)
 	})
 }
 
 func TestAdd(t *testing.T){
-	dictionary := Dictionary{}
-	word := "test"
-	definition := "this is just a test"
 
-	dictionary.Add(word, definition)
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
 
-	assertDefinition(t, dictionary, word, definition)
+		dictionary.Add(word, definition)
+
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		defenition := "this is just a test"
+		dictionary := Dictionary{word: defenition}
+		err := dictionary.Add(word, "new test")
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, defenition)
+	})
 
 
 }
