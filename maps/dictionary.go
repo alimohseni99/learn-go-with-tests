fd909970c368eaab1e6a038a65b3ffe044938306
Ali Mohseni <ali.mohseni05@yahoo.se>
2025-05-17 13:53:16 +0200

    Refactor dictionary error handling; update Update function to return appropriate errors and improve test coverage for existing and new words

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,22 +1,19 @@
 package maps
 
-import (
-	"errors"
-)
-
 type Dictionary map[string]string
 type DictionaryErr string
 
-var (
-	ErrNotFound   = errors.New("could not find the word you were looking for")
-	ErrWordExists = errors.New("cannot add word because it already exists")
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot perform operation on word because it does not exist")
 )
 
 func (e DictionaryErr) Error() string {
 	return string(e)
 }
 
-func (d Dictionary) Search(word string)(string, error) {
+func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
 	if !ok {
 		return "", ErrNotFound
@@ -24,17 +21,32 @@ func (d Dictionary) Search(word string)(string, error) {
 	return definition, nil
 }
 
-func (d Dictionary) Add(word, definition string)	error{
+func (d Dictionary) Add(word, definition string) error {
 
-		_, err := d.Search(word)
+	_, err := d.Search(word)
 
-		switch err {
-			case ErrNotFound:
-					d[word] = definition
-			case nil:
-					return ErrWordExists
-			default:
-					return err
-		}
-				return nil
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+	return nil
 }
